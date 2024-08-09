ded3a3735a6767d5630377bc0cf41ccedc888688
Adwaith M Nambiar <mnadwaith@gmail.com>
2024-08-09 18:26:53 +0530

    banner done

--- src/component/Home/BannerDisplay.js
+++ src/component/Home/BannerDisplay.js
@@ -3,17 +3,15 @@ import React from 'react';
 const BannerDisplay = ({ bannerData }) => {
 
     if (bannerData) {
-        return bannerData.map((item, index) => {
-
-            return (
-                <>
-                    <div key={item.id || index} className="carousel-item active" data-bs-interval="2000">
+        return (
+            <>
+                {bannerData.map((item, index) => (
+                    <div key={item.id || index} className={`carousel-item ${index === 0 ? 'active' : ''}`} data-bs-interval="2000">
                         <img src={item.image} className="d-block w-100" alt="offer banner" />
                     </div>
-
-                </>
-            )
-        })
+                ))}
+            </>
+        )
     }
 }
 
--- src/component/Home/QuickDisplay.js
+++ src/component/Home/QuickDisplay.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 const QuickDisplay = ({ searchData }) => {
     if (searchData) {
-        return searchData.map((item, index) => {
-            return (
-                <div className="col-lg-4" key={item.id || index}>
-                    <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
-                    <h2 className="fw-normal">{item.name}</h2>
-                    <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-                    <p><a className="btn btn-secondary" href="#">View »</a></p>
-                </div>
-            )
-        })
+        return (
+            <>
+                {searchData.map((item, index) => (
+                    <div className="col-lg-4" key={item.id || index}>
+                        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+                        <h2 className="fw-normal">{item.name}</h2>
+                        <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+                        <p><a className="btn btn-secondary" href="#">View »</a></p>
+                    </div>
+
+                ))}
+            </>
+        )
     }
 }
 
