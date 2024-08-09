9a492feade48beb7874d5a51f304c3c5cd65f000
Adwaith M Nambiar <mnadwaith@gmail.com>
2024-08-09 23:03:03 +0530

    nothing

--- src/component/Home/QuickDisplay.js
+++ src/component/Home/QuickDisplay.js
@@ -9,7 +9,7 @@ const QuickDisplay = ({ searchData }) => {
                         <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
                         <h2 className="fw-normal">{item.name}</h2>
                         <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-                        <p><a className="btn btn-secondary" href="#">View »</a></p>
+                        <p><a className="btn btn-secondary" href="#0">View »</a></p>
                     </div>
 
                 ))}
